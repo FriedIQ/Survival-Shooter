68bd175e1115708e88a75ed500035cfae2674c3e
Christian <fried.iq@gmail.com>
2015-12-09 22:17:58 -0800

    Initial Commint

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float _speed = 6f;
+
+    Vector3 _movement;
+    Animator _animator;
+    Rigidbody _ridgidbody;
+    int _floorMask;
+    float _camRayLength = 100f;
+
+    void Awake()
+    {
+        _floorMask = LayerMask.GetMask("Floor");
+        _animator = GetComponent<Animator>();
+        _ridgidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turn();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        _movement.Set(h, 0f, v);
+        _movement = _movement.normalized * _speed * Time.deltaTime;
+
+        _ridgidbody.MovePosition(transform.position + _movement);
+    }
+
+    void Turn()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+        if(Physics.Raycast(camRay, out floorHit, _camRayLength, _floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion rotation = Quaternion.LookRotation(playerToMouse);
+            _ridgidbody.MoveRotation(rotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+
+        _animator.SetBool("IsWalking", walking);
+    }
+}
